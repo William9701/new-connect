21041dfe10ca962b6f31a45ec109ce3313c76d52
Obi William <williamobi818@gmail.com>
2024-01-12 11:26:38 +0100

    updated the save and share button functions

--- web_dynamic/static/scripts/play-video.js
+++ web_dynamic/static/scripts/play-video.js
@@ -52,3 +52,68 @@ function dislike(content_id, user_id) {
       });
   });
 }
+
+function Save(content_id) {
+  event.preventDefault();
+  // Fetch the video file details from the server
+  fetch(`http://127.0.0.1:5001/api/v1/contents/${content_id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      var contentUrl = data.content; // Get the relative URL of the video file from the JSON data
+
+      // Remove the '..' from the relative URL
+      contentUrl = contentUrl.replace("..", "");
+
+      // Convert the relative URL to an absolute URL
+      var a = document.createElement("a");
+      a.href = contentUrl;
+      var absoluteUrl = a.href;
+
+      // Fetch the video file
+      fetch(absoluteUrl)
+        .then((response) => response.blob()) // Get a Blob representing the video data
+        .then((blob) => {
+          var url = window.URL.createObjectURL(blob);
+          var a = document.createElement("a");
+          a.href = url;
+          a.download = `Connect_${data.description}.mp4`; // Specify the filename
+          a.click(); // Simulate the click
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementById("close");
+
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementById("close");
+  var copy = document.getElementById("copy");
+  var copy_text = document.getElementById("copy_text");
+
+  btn.onclick = function () {
+    event.preventDefault();
+    modal.style.display = "block";
+    copy_text.textContent = window.location.href;
+  };
+
+  span.onclick = function () {
+    event.preventDefault();
+    modal.style.display = "none";
+  };
+
+  copy.onclick = function () {
+    event.preventDefault();
+    navigator.clipboard.writeText(copy_text.textContent);
+    alert("Copied the text: " + copy_text.textContent);
+  };
+
+  window.onclick = function (event) {
+    event.preventDefault();
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+});
